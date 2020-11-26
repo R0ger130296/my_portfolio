c9ad537da05238e60695ec2eb0f71eabfdb62d3c
Johao Perlaza <perlazajohao@gmail.com>
2020-11-26 13:41:51 -0500

    UI/UX experience updated knowledge base component

--- pages/knowledgebase/components/GetAllPosts.jsx
+++ pages/knowledgebase/components/GetAllPosts.jsx
@@ -33,40 +33,34 @@ export default function GetAllPosts() {
           />
           <meta charSet="UTF-8" />
         </Head>
-        <img
-          className="w-32 h-32 animate-spin"
-          alt="loading"
-          src="/vimhash.webp"
-        />
-        <h1 className="animate-pulse uppercase  ">loading...</h1>
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
       </div>
     );
   } else {
     return (
-      <div className="flex flex-wrap items-center justify-center px-4 bg-gray-300">
+      <section className="flex flex-row flex-wrap mx-auto">
         {allPosts.map((post) => (
           <div
-            className="p-4 max-w-xs mb-6 flex flex-col justify-center items-center"
+            className="transition-all duration-150 flex w-full px-4 py-6 md:w-1/2 lg:w-1/3"
             key={post.post_sec}
           >
-            <img
-              alt="pic"
-              className="bg-white h-56 w-full rounded-lg shadow-md bg-cover bg-center"
-              src={post.post_pic}
-            />
-            <div className="bg-white shadow-lg rounded-lg overflow-hidden p-5">
-              <div className="flex items-center">
-                <div className="h-2 w-2 rounded-full m-1 bg-purple-500"></div>
-                <div className="category-title flex-1 text-sm">
-                  {post.post_dat}
-                </div>
+            <div className="flex flex-col items-stretch min-h-full pb-4 mb-6 transition-all duration-150 bg-white rounded-lg shadow-lg hover:shadow-2xl border">
+              <div className="md:flex-shrink-0">
+                <img
+                  src={post.post_pic}
+                  alt={"pic" + post.post_sec}
+                  className="object-fill w-full rounded-lg rounded-b-none md:h-56"
+                />
               </div>
-              <div className="font-bold text-xl">{post.post_tit}</div>
-
-              <div className="summary-post text-base text-justify">
-                {post.post_sum}
+              <div className="flex items-center justify-between px-4 py-2 overflow-hidden">
+                <span className="text-xs font-medium text-blue-600 uppercase">
+                  #technology {/* Grupo. Ej: Programación, Cocina */}
+                </span>
+              </div>
+              <hr className="border-gray-300" />
+              <div className="flex flex-wrap items-center flex-1 px-4 py-1 text-center mx-auto">
                 <button
-                  className="bg-blue-100 text-blue-500 mt-4 block rounded p-2 text-sm"
+                  className="hover:underline"
                   onClick={() =>
                     router.push({
                       pathname: "/knowledgebase/publicationContent",
@@ -74,14 +68,42 @@ export default function GetAllPosts() {
                     })
                   }
                 >
-                  <span>Read More</span>
-                  <i className="fas fa-chevron-right px-2"></i>
+                  <h2 className="text-2xl font-bold tracking-normal text-gray-800">
+                    {post.post_tit} {/* Titulo del Tema */}
+                  </h2>
                 </button>
               </div>
+              <hr className="border-gray-300" />
+              <p className="flex flex-row flex-wrap w-full px-4 py-2 overflow-hidden text-sm text-justify text-gray-700">
+                {post.post_sum} {/* Presentación del Tema */}
+              </p>
+              <hr className="border-gray-300" />
+              <section className="px-4 py-2 mt-2">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center flex-1">
+                    <img
+                      className="object-cover h-10 rounded-full"
+                      src="/portfolio/profile-pic.png"
+                      alt="Avatar"
+                    />
+                    <div className="flex flex-col mx-2">
+                      <a
+                        href=""
+                        className="font-semibold text-gray-700 hover:underline"
+                      >
+                        Johao Perlaza {/* Autor */}
+                      </a>
+                      <span className="mx-1 text-xs text-gray-600">
+                        Last update: {post.post_dat} {/* Fecha */}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </section>
             </div>
           </div>
         ))}
-      </div>
+      </section>
     );
   }
 }
--- pages/knowledgebase/components/GetPost.jsx
+++ pages/knowledgebase/components/GetPost.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 
+import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
 
 import { db } from "../../../services/_firebase";
@@ -9,6 +10,7 @@ import { db } from "../../../services/_firebase";
 export default function GetPost({ id }) {
   const [loading, setLoading] = useState(true),
     [post, setPost] = useState([]),
+    [user, setUser] = useState([]),
     router = useRouter();
 
   useEffect(() => {
@@ -28,6 +30,11 @@ export default function GetPost({ id }) {
         }
       });
     });
+
+    axios.get(`https://api.github.com/users/vimhash`).then((response) => {
+      console.log(response.data);
+      setUser(response.data);
+    });
   }, []);
 
   if (loading) {
@@ -42,19 +49,14 @@ export default function GetPost({ id }) {
           />
           <meta charSet="UTF-8" />
         </Head>
-        <img
-          className="w-32 h-32 animate-spin"
-          alt="loading"
-          src="/vimhash.webp"
-        />
-        <h1 className="animate-pulse uppercase  ">loading...</h1>
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
       </div>
     );
   } else {
     return (
-      <div className="flex flex-wrap items-center justify-center px-4 bg-gray-300">
+      <div className="flex">
         {post.map((post) => (
-          <div className="my-1 mx-1 lg:my-4 lg:mx-4 w-full" key={post.post_sec}>
+          <main className="w-4/5 mt-1">
             <Head>
               <title>Johao Perlaza - {post.post_tit} </title>
               <meta name="description" content={post.post_con} />
@@ -64,32 +66,83 @@ export default function GetPost({ id }) {
               />
               <meta charSet="UTF-8" />
             </Head>
-            <div className="flex lg:flex-row md:flex-row sm:flex-row flex-col items-center">
-              <img alt="pic" className="h-64 w-64" src={post.post_pic} />
-              <div>
-                <div className="flex flex-col leading-tight p-2 md:p-4">
-                  <h1 className="text-4xl">{post.post_tit}</h1>
-                  <p className="text-gray-500 text-sm">{post.post_dat}</p>
-                </div>
-
-                <div className="flex items-center justify-between leading-tight p-2 md:p-4 text-gray-700">
-                  {post.post_sum}
+            <div className="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative">
+              <img
+                src={post.post_pic}
+                className="left-0 top-0 w-full h-full z-0 object-cover opacity-25 max-h-80"
+              />
+              <div className="p-4 absolute bottom-0 left-0 z-20">
+                <a className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">
+                  #technology
+                </a>
+                <h2 className="text-4xl font-bold leading-tight">
+                  {post.post_tit}
+                </h2>
+                <div className="flex mt-3">
+                  <img
+                    src="/portfolio/profile-pic.png"
+                    className="h-10 w-10 rounded-full mr-2 object-cover"
+                  />
+                  <div>
+                    <p className="font-semibold text-sm">Johao Perlaza</p>
+                    <p className="font-semibold text-xs">{post.post_dat}</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="leading-tight p-2 md:p-4">
-              {ReactHtmlParser(post.post_con)}
+
+            <div className="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
+              <p className="pb-6">{ReactHtmlParser(post.post_con)}</p>
+            </div>
+          </main>
+        ))}
+        <div className="w-1/5 px-4">
+          {/* <div
+            id="app"
+            className="max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4 mx-auto"
+          >
+            <github-user-card username="vimhash"></github-user-card>
+          </div> */}
+          <div
+            className="cursor-pointer border mt-1"
+            onClick={() => window.location.assign(user.blog)}
+          >
+            <div className="flex items-center px-6 py-3 bg-gray-900">
+              <i className="fab fa-github text-2xl text-white"></i>
+              <h1 className="mx-3 text-white font-semibold text-mg">
+                Github Account
+              </h1>
+            </div>
+            <img
+              className="w-full h-56 object-cover object-center"
+              src={user.avatar_url}
+              alt="avatar"
+            />
+            <div className="flex items-center px-6 py-3 bg-gray-900">
+              <i className="fas fa-eye text-2xl text-white"></i>
+              <h1 className="mx-3 text-white font-semibold text-mg">
+                {user.public_repos} Public Proyects
+              </h1>
+            </div>
+            <div className="py-4 px-6">
+              <h1 className="text-2xl font-semibold text-gray-800">
+                {user.name}
+              </h1>
+              <p className="py-2 text-lg text-gray-700">{user.bio}</p>
+              <div className="flex items-center mt-4 text-gray-700">
+                <svg className="h-6 w-6 fill-current" viewBox="0 0 20 20">
+                  <path d="M15.573,11.624c0.568-0.478,0.947-1.219,0.947-2.019c0-1.37-1.108-2.569-2.371-2.569s-2.371,1.2-2.371,2.569c0,0.8,0.379,1.542,0.946,2.019c-0.253,0.089-0.496,0.2-0.728,0.332c-0.743-0.898-1.745-1.573-2.891-1.911c0.877-0.61,1.486-1.666,1.486-2.812c0-1.79-1.479-3.359-3.162-3.359S4.269,5.443,4.269,7.233c0,1.146,0.608,2.202,1.486,2.812c-2.454,0.725-4.252,2.998-4.252,5.685c0,0.218,0.178,0.396,0.395,0.396h16.203c0.218,0,0.396-0.178,0.396-0.396C18.497,13.831,17.273,12.216,15.573,11.624 M12.568,9.605c0-0.822,0.689-1.779,1.581-1.779s1.58,0.957,1.58,1.779s-0.688,1.779-1.58,1.779S12.568,10.427,12.568,9.605 M5.06,7.233c0-1.213,1.014-2.569,2.371-2.569c1.358,0,2.371,1.355,2.371,2.569S8.789,9.802,7.431,9.802C6.073,9.802,5.06,8.447,5.06,7.233 M2.309,15.335c0.202-2.649,2.423-4.742,5.122-4.742s4.921,2.093,5.122,4.742H2.309z M13.346,15.335c-0.067-0.997-0.382-1.928-0.882-2.732c0.502-0.271,1.075-0.429,1.686-0.429c1.828,0,3.338,1.385,3.535,3.161H13.346z"></path>{" "}
+                </svg>
+                <h1 className="px-2 text-sm">{user.followers} Seguidores</h1>
+              </div>
+              <div className="flex items-center mt-4 text-gray-700">
+                <svg className="h-6 w-6 fill-current" viewBox="0 0 20 20">
+                  <path d="M10,1.375c-3.17,0-5.75,2.548-5.75,5.682c0,6.685,5.259,11.276,5.483,11.469c0.152,0.132,0.382,0.132,0.534,0c0.224-0.193,5.481-4.784,5.483-11.469C15.75,3.923,13.171,1.375,10,1.375 M10,17.653c-1.064-1.024-4.929-5.127-4.929-10.596c0-2.68,2.212-4.861,4.929-4.861s4.929,2.181,4.929,4.861C14.927,12.518,11.063,16.627,10,17.653 M10,3.839c-1.815,0-3.286,1.47-3.286,3.286s1.47,3.286,3.286,3.286s3.286-1.47,3.286-3.286S11.815,3.839,10,3.839 M10,9.589c-1.359,0-2.464-1.105-2.464-2.464S8.641,4.661,10,4.661s2.464,1.105,2.464,2.464S11.359,9.589,10,9.589"></path>{" "}
+                </svg>
+                <h1 className="px-2 text-sm">{user.location}</h1>
+              </div>
             </div>
           </div>
-        ))}
-        <div className="m-3">
-          <button
-            className="bg-white text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
-            onClick={() => router.push("/knowledgebase")}
-          >
-            <span className="mr-2">Go back</span>
-            <i className="fas fa-undo"></i>
-          </button>
         </div>
       </div>
     );
